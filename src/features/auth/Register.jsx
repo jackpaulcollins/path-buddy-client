9b8c39abd19fcb2b39ac8a324c99cf24c2c7248c
Jack Collins <jackpaulcollins@gmail.com>
2023-10-29 10:46:52 -0700

    remove stock images

--- src/features/auth/Register.jsx
+++ src/features/auth/Register.jsx
@@ -114,11 +114,6 @@ function Login() {
   const content = (
     <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
         <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign up for an account
         </h2>
