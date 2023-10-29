89304a0631d3f7b55398b1a77ed9cf944f50f9d0
Jack Collins <jackpaulcollins@gmail.com>
2023-10-29 10:50:37 -0700

    remove stock pics

--- src/components/authenticated/dashboard/Navbar.jsx
+++ src/components/authenticated/dashboard/Navbar.jsx
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { logOut, selectCurrentUser } from '../../../features/auth/authSlice';
+import DefaultUserPicture from '../../../assets/icons/DefaultUserPicture'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -39,13 +40,6 @@ function Navbar() {
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
               <div className="flex h-16 items-center justify-between">
                 <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                      alt="Your Company"
-                    />
-                  </div>
                   <div className="hidden sm:ml-6 sm:block">
                     <div className="flex space-x-4">
                       <Link
@@ -86,11 +80,7 @@ function Navbar() {
                         <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                           <span className="absolute -inset-1.5" />
                           <span className="sr-only">Open user menu</span>
-                          <img
-                            className="h-8 w-8 rounded-full"
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt=""
-                          />
+                          <DefaultUserPicture/>
                         </Menu.Button>
                       </div>
                       <Transition
@@ -190,11 +180,7 @@ function Navbar() {
               <div className="border-t border-gray-700 pb-3 pt-4">
                 <div className="flex items-center px-5">
                   <div className="flex-shrink-0">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
+                    <DefaultUserPicture/>
                   </div>
                   <div className="ml-3">
                     <div className="text-base font-medium text-white">
